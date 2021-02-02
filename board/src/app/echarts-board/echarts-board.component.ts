7d0086cb311c09d6db8c9d857f1bbfb29bad8a03
wangcong <2330630904@qq.com>
2021-02-02 14:11:59 +0800

    init

---
+++ board/src/app/echarts-board/echarts-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-echarts-board',
+  templateUrl: './echarts-board.component.html',
+  styleUrls: ['./echarts-board.component.scss']
+})
+export class EchartsBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
