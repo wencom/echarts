36ded8191517f4be1cdb2a10ecdb87fd91c30f80
wangcong <2330630904@qq.com>
2021-02-02 15:17:40 +0800

    1.update echarts version 2.introduce NgxEchartsModule 3.a test

--- board/src/app/routes/routes.module.ts
+++ board/src/app/routes/routes.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RoutesRoutingModule } from './routes-routing.module';
 import { EchartsBoardComponent } from './echarts-board/echarts-board.component';
+import { NgxEchartsModule } from "ngx-echarts";
 
 
 @NgModule({
   declarations: [EchartsBoardComponent],
   imports: [
     CommonModule,
-    RoutesRoutingModule
+    RoutesRoutingModule,
+    NgxEchartsModule
   ]
 })
 export class RoutesModule { }
